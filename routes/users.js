a3ef86ef493930246a1e23389396a17238f0361d
edin <edo.truman@gmail.com>
2021-02-21 11:39:06 +0100

    post method

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,29 @@ import express from "express"
 
 const router = express.Router()
 
+const users = [
+  {
+    Name: "Kyrie Irving",
+    Team: "Brooklyn Nets",
+    Points: 33
+  },
+  {
+    Name: "Damien Lillard",
+    Team: "Portland",
+    Points: 38
+  }
+]
+
 // all routes starts with /users
 router.get("/", (req, res) => {
-  res.send("Sha mai?")
+  res.send(users)
+})
+
+router.post("/", (req, res) => {
+  // console.log(req.body)
+  const user = req.body
+  users.push(user)
+  res.send(`User with the name ${user.Name} added to the database`)
 })
 
 export default router
